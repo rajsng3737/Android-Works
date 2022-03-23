25e8c7907220c8177d03fdb823fa8fd3052aef36
Giriraj Singh <rajsng3737@gmail.com>
2022-03-24 00:25:15 +0530

    added more humidity and wind speed

--- WeatherWeather/app/src/main/java/com/example/weatherweather/MainActivity.java
+++ WeatherWeather/app/src/main/java/com/example/weatherweather/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -30,32 +32,50 @@ public class MainActivity extends AppCompatActivity {
         String cityName = String.valueOf(editText.getText());
         new theBackgroundClass().execute(cityName);
         }
-       class theBackgroundClass extends AsyncTask<String,String,String>{
+        class theBackgroundClass extends AsyncTask<String,String,String>{
+            ProgressBar progressBar = findViewById(R.id.progressBar);
            @Override
-       protected String doInBackground(String... strings) {
-           try {
-               URL url = new URL("https://pro.openweathermap.org/data/2.5/weather?q="+strings[0]+"&APPID=b1b15e88fa797225412429c1c50c122a1");
-               HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-               InputStream in = url.openStream();
-               BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-               StringBuilder result = new StringBuilder();
-               String line;
-               while((line = reader.readLine()) != null) {
-                   result.append(line);
+           protected String doInBackground(String... strings) {
+               try {
+                   URL url = new URL("https://pro.openweathermap.org/data/2.5/weather?q="+strings[0]+"&APPID=b1b15e88fa797225412429c1c50c122a1");
+                   HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                   InputStream in = url.openStream();
+                   BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                   StringBuilder result = new StringBuilder();
+                   String line;
+                   while((line = reader.readLine()) != null) {
+                       result.append(line);
+                   }
+                   JSONObject responseInJSON = new JSONObject(result.toString());
+                   if(responseInJSON.get("cod").toString().equals("404"))
+                        publishProgress( "CityNotFound");
+                   else if(responseInJSON.get("cod").toString().equals("200"))
+                   {
+                       JSONObject main = responseInJSON.getJSONObject("main");
+                       JSONObject wind = responseInJSON.getJSONObject("wind");
+                       publishProgress( main.get("temp").toString(), main.get("humidity").toString() , wind.get("speed").toString());
+                   }
+               } catch (JSONException | IOException e) {
+                   e.printStackTrace();
+                   publishProgress("Error :\"\"(");
                }
-               JSONObject responseInJSON = new JSONObject(result.toString());
-               JSONObject main = responseInJSON.getJSONObject("main");
-               publishProgress(main.get("temp").toString());
-           } catch (JSONException | IOException e) {
-               e.printStackTrace();
+              return "";
            }
-          return "";
-       }
-         @Override
-         protected void onProgressUpdate(String... values) {
-           TextView weatherInfo = findViewById(R.id.weatherInfo);
-           weatherInfo.setText(values[0]);
-         }
-     }
-
+           @Override
+             protected void onProgressUpdate(String[] values) {
+               progressBar.setVisibility(View.GONE);
+               TextView weatherInfo = findViewById(R.id.weatherInfo);
+               StringBuilder info = new StringBuilder();
+               for(int i = 0; i<3; i++)
+                   if(i < values.length )
+                   {
+                       info.append(values[i]).append("\n");
+                   }
+               weatherInfo.setText(info.toString());
+             }
+            @Override
+            protected void onPreExecute() {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+        }
 }
