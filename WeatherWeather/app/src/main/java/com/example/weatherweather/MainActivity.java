c4cd1c5d6c0276a208a65d24598e11b1b7ed6729
Giriraj Singh <rajsng3737@gmail.com>
2022-03-24 14:32:10 +0530

    added humidity and wind speed

--- WeatherWeather/app/src/main/java/com/example/weatherweather/MainActivity.java
+++ WeatherWeather/app/src/main/java/com/example/weatherweather/MainActivity.java
@@ -14,7 +14,6 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
